98fe4abcf47a399cb93b04a55e88e2b01c31eca3
vkr-02 <vaibhavikr02@gmail.com>
2026-08-30 17:30:44 +0530

    feat: add task model

---
+++ backend/models/Task.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+
+        description: {
+            type: String
+        },
+
+        status: {
+            type: String,
+            enum: ["todo", "in-progress", "completed"],
+            default: "todo"
+        },
+        
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
