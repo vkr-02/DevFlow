d0b0a51fc01c4d3f24e20d9eadd326c3fc529d59
vkr-02 <vaibhavikr02@gmail.com>
2026-09-05 22:01:05 +0530

    feat: add frontend login and JWT storage

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
+function Login () {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://localhost:5000/api/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type" : "application/json"
+                },
+                body: JSON.stringify({
+                    email,
+                    password
+                })
+            });
+            const data = await response.json();
+            setMessage(data.message);
+
+            if(response.ok) {
+                localStorage.setItem("token", data.token);
+                navigate("/tasks");
+            }
+        } catch (error) {
+            setMessage("Unable to connect to server");
+            console.log("Login error:", error);
+            
+        }
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+
+            <form onSubmit={handleSubmit}>
+                <input type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)} 
+                />
+                <input type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)} 
+                />
+                <button type="submit">Login</button>
+            </form>
+            <p>{message}</p>
+        </div>
+    )
 }
 
-export default Login
+export default Login;
